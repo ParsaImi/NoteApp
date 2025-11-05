297b6c6f06c92b1567a95bc0825a2accfbdf0052
ParsaImi <parsaemani17@gmail.com>
2025-11-05 17:37:40 +0330

    fix: emoji

--- release.config.js
+++ release.config.js
@@ -3,10 +3,30 @@ module.exports = {
   repositoryUrl: 'file://' + __dirname + '/.git',  // Points to your local Git repo to bypass remote origin checks
   ci: false,  // Explicitly disable CI mode for local runs (equivalent to --no-ci flag)
   dryRun: false,  // Set to true if you want dry-run baked in; otherwise use CLI flag
+  tagFormat: `v${'${version}'}`,  // Format of the git tag (e.g., v1.0.0)
 
   plugins: [
-    '@semantic-release/commit-analyzer',  // Analyzes commits for version bump
-    '@semantic-release/release-notes-generator',  // Generates release notes
+    [
+     '@semantic-release/commit-analyzer',  // Analyzes commits for version bump
+     {
+     	preset: 'angular',
+	releaseRules: [
+	  { type: 'docs', release: false },
+	  { type: 'refactor', release: 'patch' },
+	  { type: 'style', release: 'patch' },
+	  { breaking: true, release: 'patch' },
+	]
+	"parserOpts": {
+	  "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES"]
+	}
+     }
+    ],
+    [
+        '@semantic-release/release-notes-generator',  // Generates release notes
+	{
+	  "preset": "emoji",
+	},
+    ],
     [
       '@semantic-release/changelog',
       {
